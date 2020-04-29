54a8197ccb1c1bae5d105254746f38e1ffe9b4b9
duane11003 <duane.mcfar@gmail.com>
2020-04-29 19:39:31 -0400

    added Favorites component. Added button to each stock to allow users to add a favorite

---
+++ app/Components/Favorites.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Favorites({favoriteStock}) {
+    return (
+        <div>
+            <p>Favorite Symbols</p>
+        </div>
+    )
+}
+
+export default Favorites
--- app/index.js
+++ app/index.js
@@ -4,6 +4,7 @@ import {token, token2} from '../API_token/config'
 import fetchData from '../utils/fetchData'
 import Search from './Components/Search'
 import './index.css'
+import Favorites from './Components/Favorites'
 
 function App () {
 
@@ -12,18 +13,24 @@ function App () {
     const [dailyLowPrice, setDailyLowPrice] = useState(null)
     const [dailyHighPrice, setDailyHighPrice] = useState(null)
     const [dailyClosingPrice, setDailyClosingPrice] = useState(null)
+    const [error, setError] = useState(false)
+    const [favoriteStock, setFavoriteStock] = useState([])
 
 function searchForSymbol (inputText) {
     const fetchUrl = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${inputText}&apikey=${token2}`
     fetchData(fetchUrl)
-     .then(res => {
+     .then(res => { 
+         if (!res) setError(true)
         const arrFromObject = Object.values(res['Time Series (Daily)'])
         const currentDayPrices = arrFromObject[arrFromObject.length-1]
         setSymbol(res['Meta Data']['2. Symbol'])
-        setDailyOpenPrice(currentDayPrices['1. open'])
-        setDailyHighPrice(currentDayPrices['2. high'])
-        setDailyLowPrice(currentDayPrices['3. low'])
-        setDailyClosingPrice(currentDayPrices['4. close'])
+        // Math.ceil(currentDayPrices['2. high'] * 100) / 100
+        setDailyOpenPrice(Math.ceil(currentDayPrices['1. open'] * 100) / 100)
+        setDailyHighPrice(Math.ceil(currentDayPrices['2. high'] * 100) / 100)
+        setDailyLowPrice(Math.ceil(currentDayPrices['3. low'] * 100) / 100)
+        setDailyClosingPrice(Math.ceil(currentDayPrices['4. close'] * 100) / 100)
+     }).catch((err) => {
+         setError('There was an error:', err)
      })
 }
 
@@ -32,11 +39,14 @@ function searchForSymbol (inputText) {
             <Search searchForSymbol={searchForSymbol} />
             <div>
                 <h3>{symbol}</h3>
+                {error === null ? <p>There was an error {error}</p> : null}
                 {dailyOpenPrice !== null ? <p> Open Price: ${dailyOpenPrice}</p> : null}
                 {dailyHighPrice !== null ? <p>High Price: ${dailyHighPrice}</p> : null}
                 {dailyLowPrice !== null ? <p>Low Price: ${dailyLowPrice}</p> : null}
                 {dailyClosingPrice !== null ? <p>Closing Price: ${dailyClosingPrice}</p> : null}
+                {symbol !== null ? <button>Add to Favorites</button> : null}
             </div>
+            <Favorites favoriteStock={favoriteStock} />
         </Fragment>
     )
 }
